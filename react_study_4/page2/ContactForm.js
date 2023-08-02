a38b3a85ffd0e242991d5d0049100d7083335e03
tedi0609 <tedysahputra692@gmail.com>
2023-08-02 14:43:30 +0800

    react study

---
+++ react_study_4/page2/ContactForm.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class ContactForm extends React.Component {
+  render() {
+    return (
+      <div className='contact-form'>
+        <form>
+          <p>Alamat Email (wajib diisi)</p>
+          {/* Tambahkan tag input */}
+          <p>Alamat Email (wajib diisi)</p>
+          <input />
+          <p>Pesan (wajib diisi)</p>
+          {/* Tambahkan tag textarea */}
+          <textarea />
+          {/* Tambahkan tombol kirim */}
+          <input type ='submit' value='Kirim' />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
---
+++ react_study_4/page4/ContactForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted: false,
+    };
+  }
+
+  /* Deklarasikan method handleSubmit  */
+  handleSubmit(){
+	this.setState({isSubmitted: true});
+}
+render(){
+	<form onSubmit={ () => {this.handleSubmit()}}>
+
+
+  render() {
+    let contactForm;
+    if (this.state.isSubmitted) {
+      contactForm = (
+        <div className='contact-submit-message'>
+          Pesan Terkirim
+        </div>
+      );
+    } else {
+      contactForm = (
+        /* Tambahkan onSubmit ke tag form */
+        <form onSubmit={() => {code}>
+          <p>Alamat Email (wajib diisi)</p>
+          <input />
+          <p>Pesan (wajib diisi)</p>
+          <textarea />
+          <input
+            type='submit'
+            value='Kirim'
+          />
+        </form>
+      );
+    }
+    
+    return (
+      <div className='contact-form'>
+        {contactForm}
+      </div>
+    );
+  }
+}
+
+export default ContactForm;
